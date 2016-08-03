c902aa4457ca9b9c4145aecb11527dd6e399194b
Kateryna Senchenko <kateryna.senchenko@gmail.com>
2016-08-03 17:31:23 +0300

    Added JS implementation of the UserService

--- webapp/src/main/webapp/js/services/userService.js
+++ webapp/src/main/webapp/js/services/userService.js
@@ -1,6 +1,59 @@
 var UserService = function (eventbus, events, baseUrl) {
 
+    var _registerUser = function (userData) {
 
+        var email = userData.email;
+        var password = userData.password;
+        var confirmPassword = userData.confirmPassword;
+
+        $.post(baseUrl + "app/registration",
+            {
+                email: email,
+                password: password,
+                confirmPassword: confirmPassword
+
+            }, function (xhr) {
+
+                var data = eval("(" + xhr + ")");
+                eventbus.post(events.USER_IS_REGISTERED, data);
+
+            }, 'text')
+
+            .fail(function (xhr) {
+
+                var data = eval("(" + xhr.responseText + ")");
+                eventbus.post(events.REGISTRATION_FAILED, data);
+            })
+    };
+
+    var _loginUser = function (userData) {
+
+        var email = userData.email;
+        var password = userData.password;
+
+        $.post(baseUrl + "app/login",
+            {
+                email: email,
+                password: password
+            },
+            function (xhr) {
+
+                var data = eval("(" + xhr + ")");
+                eventbus.post(events.USER_IS_LOGGED_IN, data);
+
+            }, 'text')
+
+            .fail(function (xhr) {
+
+                var data = eval("(" + xhr.responseText + ")");
+                eventbus.post(events.LOGIN_FAILED, data);
+            })
+    };
+
+    return {
+        "registerUser": _registerUser,
+        "loginUser": _loginUser
+    };
 };
 
 if (typeof define !== 'function') {
